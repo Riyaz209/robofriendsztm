14bbd949351dc4e25a322b2f9cba0f9402d8bd31
Riyaz <iamriyaz100@gmail.com>
2021-01-20 13:56:09 +0000

    Added Error Boundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+	/*if(true) {
+		throw new Error('Noooooo!');
+	}*/
 	/*const cardComponent = robots.map((user, i) => {
 	return ( <Card key={user.id} id={robots[i].id} name={user.name} email={robots[i].email} /> );
 	});*/
